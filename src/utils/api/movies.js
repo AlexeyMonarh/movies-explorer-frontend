8c5f5379e17c1185114f37ae7b4481d86e18eb3f
AlexeyMonarh <monarhweb@gmail.com>
2021-03-28 15:55:00 +0300

    level-2-adaptiv

---
+++ src/utils/api/movies.js
@@ -0,0 +1,85 @@
+import slova from '../../images/imagecompressor/33slova-min.png';
+import stoLet from '../../images/imagecompressor/100let-min.png';
+import vPogone from '../../images/imagecompressor/v-pogone-min.png';
+import baskiay from '../../images/imagecompressor/baskiay-min.png';
+import beg from '../../images/imagecompressor/beg-min.png';
+import knigo from '../../images/imagecompressor/knigo-min.png';
+import dymauOGermany from '../../images/imagecompressor/dymau-o-germany-min.png';
+import gimDanger from '../../images/imagecompressor/gim-danger-min.png';
+import djenis from '../../images/imagecompressor/djenis-min.png';
+import soberis from '../../images/imagecompressor/soberis-min.png';
+import piJHarvi from '../../images/imagecompressor/pi-j-harvi-min.png';
+import poVolnam from '../../images/imagecompressor/po-volnam-min.png';
+import roodboy from '../../images/imagecompressor/roodboy-min.png';
+import skate from '../../images/imagecompressor/skate-min.png';
+import voina from '../../images/imagecompressor/voina-min.png';
+import zona from '../../images/imagecompressor/zona-min.png';
+
+const items = [
+  {
+    img: slova,
+    description: '33 слова о дизайне',
+  },
+  {
+    img: stoLet,
+    description: 'Киноальманах «100 лет дизайна»',
+  },
+  {
+    img: vPogone,
+    description: 'В погоне за Бенкси',
+  },
+  {
+    img: baskiay,
+    description: 'Баския: Взрыв реальности',
+  },
+  {
+    img: beg,
+    description: 'Бег это свобода',
+  },
+  {
+    img: knigo,
+    description: 'Книготорговцы',
+  },
+  {
+    img: dymauOGermany,
+    description: 'Когда я думаю о Германии ночью',
+  },
+  {
+    img: gimDanger,
+    description: 'Gimme Danger: История Игги и The Stooges',
+  },
+  {
+    img: djenis,
+    description: 'Дженис: Маленькая девочка грустит',
+  },
+  {
+    img: soberis,
+    description: 'Соберись перед прыжком',
+  },
+  {
+    img: piJHarvi,
+    description: 'Пи Джей Харви: A dog called money',
+  },
+  {
+    img: poVolnam,
+    description: 'По волнам: Искусство звука в кино',
+  },
+  {
+    img: roodboy,
+    description: 'Рудбой',
+  },
+  {
+    img: skate,
+    description: 'Скейт — кухня',
+  },
+  {
+    img: voina,
+    description: 'Война искусств',
+  },
+  {
+    img: zona,
+    description: 'Зона',
+  },
+];
+
+export default items;
